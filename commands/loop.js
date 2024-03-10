dd99900b7de1fc4cad3f43990a5ae677861643b3
taviao <96363628+taviao@users.noreply.github.com>
2024-03-10 19:17:12 -0300

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,22 +1,7 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
 const db = require("../mongoDB");
 module.exports = {
   name: "loop",
-  description: "Turns the music loop mode on or off.",
+  description: "Ativa ou desativa o modo loop.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -25,7 +10,7 @@ module.exports = {
     try {
       const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ Sem música tocando!!', ephemeral: true }).catch(e => { })
   
       let button = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
@@ -49,7 +34,7 @@ module.exports = {
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=65182bf4&is=6516da74&hm=9ae58f40fcea5dc42a2a992bbd159d50116b3bafe5c5f7728e3a5276442efd2a&', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-        .setDescription('**Looping it! Let the music play on and on. **')
+        .setDescription('**Loop ativado! Deixe a música tocar sem parar. **')
      
       interaction?.reply({ embeds: [embed], components: [button], fetchReply: true }).then(async Message => {
 
@@ -60,7 +45,7 @@ module.exports = {
           if (button.user.id !== interaction.user.id) return
           const queue1 = client.player.getQueue(interaction.guild.id);
           if (!queue1 || !queue1.playing) {
-            await interaction?.editReply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+            await interaction?.editReply({ content: '⚠️ Sem música tocando!!', ephemeral: true }).catch(e => { })
             await button?.deferUpdate().catch(e => {})
           }
           switch (button.customId) {
@@ -71,13 +56,13 @@ module.exports = {
               break
             case 'nowplaying':
               const success2 = queue.setRepeatMode(1);
-              interaction?.editReply({ content: `✅ Looping activated!!` }).catch(e => { })
+              interaction?.editReply({ content: `✅ Looping Ativado!!` }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
             case 'close':
               if (queue.repeatMode === 0) {
                 await button?.deferUpdate().catch(e => {})
-                return interaction?.editReply({ content: '⚠️ Looping already Off!!', ephemeral: true }).catch(e => { })
+                return interaction?.editReply({ content: '⚠️ Looping Off!!', ephemeral: true }).catch(e => { })
               }
               const success4 = queue.setRepeatMode(0);
               interaction?.editReply({ content: '▶️ Looping off' }).catch(e => { })
@@ -107,18 +92,3 @@ module.exports = {
   }
   }
 }
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
